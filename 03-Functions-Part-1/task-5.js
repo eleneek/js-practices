c127b1513d0f068e8886f111b3760f1574ec0cb2
eleneek <elene.kvaratskhelia.1@btu.edu.ge>
2019-05-29 14:39:21 +0400

    fix(03-Functions-Part-1): Fix task-5

--- 03-Functions-Part-1/task-5.js
+++ 03-Functions-Part-1/task-5.js
@@ -1,24 +1,24 @@
-let arr1 = [1, 2, -4, 3, -9, -1, 7];
-let arr2 = [];
-function f(arr) {
-    for (let i of arr1) {
-        if (i>0){
-            arr2.push(i)
-        }
-    }
-    console.log(arr2);
-}
-f(arr1);
-
 function isPositive(num) {
     if (typeof num !== "number"){
         throw new Error('parameter type is not a Number')
     }
     else if (num >0) {
-        console.log(true); 
+        return true; 
     }
     else {
-        console.log(false);
+        return false;
     }
 }
-isPositive('s')
+
+let arr1 = [1, 2, -4, 3, -9, -1, 7];
+let arr2 = [];
+
+for (let i of arr1) {
+    if(isPositive(i)) {
+        arr2.push(i)
+    }
+}
+console.log(arr2);
+console.log(isPositive(-3));
+console.log(isPositive(3));
+
