a76056e291ca87a608323eacb9353f777cb2f1f7
eleneek <elene.kvaratskhelia.1@btu.edu.ge>
2019-06-04 15:22:38 +0400

    fix(08-Getting-in-touch-with-objects): Fix tasks

--- 08-Getting-in-touch-with-objects/task-2.js
+++ 08-Getting-in-touch-with-objects/task-2.js
@@ -8,16 +8,20 @@ Object.defineProperties(person, {
 
     },
     'salary' :{
-        configurable: false,
         get () {
             let day = new Date().getDate();    
             let sum = day * person.rate;
     
             return sum
-        }
+        },
+
+        set (value) {
+            throw new Error('You can\'t change salary');
+        },
+
+        
     }
 });
-
 person.rate = 6;
 
 console.log(person.salary);
