33ffe2623ffc7221d49a949e71b5e1761dce4a40
eleneek <elene.kvaratskhelia.1@btu.edu.ge>
2019-06-07 16:19:05 +0400

    fix(11-OOP-Part2): Fix task-2

--- 11-Object-Oriented-Programming-Part-2/task-2.js
+++ 11-Object-Oriented-Programming-Part-2/task-2.js
@@ -1,7 +1,7 @@
 function CoffeeMachine(power) {
 	this.power = power;
+	this.waterAmount = 0;
 }
-CoffeeMachine.prototype.waterAmount = 0;
 CoffeeMachine.prototype.WATER_HEAT_CAPACITY = 4200;
 CoffeeMachine.prototype.getTimeToBoil = function() {
 	return  this.waterAmount * this.WATER_HEAT_CAPACITY * 80 / this.power;
