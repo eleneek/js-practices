5c64391153f66deb3b1851a24d0fa81d88d799e1
eleneek <elene.kvaratskhelia.1@btu.edu.ge>
2019-06-07 16:07:55 +0400

    fix(11-OOP-Part2): Fix task-2

--- 11-Object-Oriented-Programming-Part-2/task-2.js
+++ 11-Object-Oriented-Programming-Part-2/task-2.js
@@ -1,23 +1,21 @@
 function CoffeeMachine(power) {
-	this.waterAmount = 0;
-	this.WATER_HEAT_CAPACITY = 4200;
-	
-    this.getTimeToBoil = function() {
-		return this.waterAmount * this.WATER_HEAT_CAPACITY * 80 / power;
-    }
-
-    CoffeeMachine.prototype.run = function () {
-        setTimeout(function() {
-			console.log('Coffee is ready!');
-    	}, this.getTimeToBoil());
-    };
-
-    CoffeeMachine.prototype.setWaterAmount = function(amount) {
-		this.waterAmount = amount;
-	};
-
+	this.power = power;
+}
+CoffeeMachine.prototype.waterAmount = 0;
+CoffeeMachine.prototype.WATER_HEAT_CAPACITY = 4200;
+CoffeeMachine.prototype.getTimeToBoil = function() {
+	return  this.waterAmount * this.WATER_HEAT_CAPACITY * 80 / this.power;
 }
 
+CoffeeMachine.prototype.run = function () {
+	setTimeout(function() {
+		console.log('Coffee is ready!');
+	}, this.getTimeToBoil());
+};
+
+CoffeeMachine.prototype.setWaterAmount = function(amount) {
+	this.waterAmount = amount;
+};
 
 let coffeeMachine = new CoffeeMachine(10000);
 coffeeMachine.setWaterAmount(50);
